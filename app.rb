550c40a5cd74cca1dad4e84f33a3de6f1fbb2ad7
henteko <henteko07@gmail.com>
2014-10-06 02:07:10 +0900

    init

---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'open-uri'
+require 'json'
+
+BASE_URL = "http://mahouka.jp/img/special/message/mahouka_msg"
+MAX_ONISAMA = 17
+
+get '/' do
+  erb :index
+end
+
+get '/api' do
+  id = rand(MAX_ONISAMA) + 1
+
+  get_json(create_url(id), id)
+end
+
+get '/api/:id' do |id|
+  id = 1 if id.to_i < 1 or id.to_i > MAX_ONISAMA
+
+  get_json(create_url(id), id)
+end
+
+def create_url(id)
+  url = BASE_URL + sprintf("%02d", id) + ".jpg"
+end
+
+def get_json(url, id)
+  content_type :json
+  { :url => url, :id => id }.to_json
+end
